78ef885039f5376acdcdc484ae32d079364e80cc
UgoRastell <urastell@normandiewebschool.fr>
2023-08-18 11:55:20 +0200

    patch create tutoriel

--- src/Controller/Admin/TutorielCrudController.php
+++ src/Controller/Admin/TutorielCrudController.php
@@ -6,11 +6,15 @@ use App\Entity\Tutoriel;
 use App\Entity\Categorie;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -36,6 +40,26 @@ class TutorielCrudController extends AbstractCrudController
             ->setEntityLabelInSingular('tutoriel')
             ->setEntityLabelInPlural('Tutoriel')
         ;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        // Ajouter une action personnalisée
+        $customAction = Action::new('customAction', 'Custom Action', 'fa fa-cogs')
+            ->linkToCrudAction('customActionHandler'); // Cette méthode sera appelée lorsque l'action est exécutée
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, $customAction); // Ajouter l'action à la page d'index
+    }
+
+    public function customActionHandler(AdminContext $context)
+    {
+        // Ici, vous pouvez exécuter votre propre logique pour l'action personnalisée
+        // Par exemple, dump les données de la requête pour voir ce qui est envoyé
+        dump($context->getRequest()->request->all());
+
+        // Vous pouvez également rediriger l'utilisateur vers une autre page
+        return $this->redirectToRoute('some_route');
     }
     
     public function configureFields(string $pageName): iterable
@@ -69,6 +93,9 @@ class TutorielCrudController extends AbstractCrudController
             
         ];
         
+        
     }
+
+    
  
 }
